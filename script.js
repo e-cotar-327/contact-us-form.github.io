e2a5b89c4f623971ee320f7402d83304471421bd
Elena Cotar <e_cotar@yahoo.com>
2024-09-14 12:43:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,123 +1,162 @@
 // Targhetam fiecare input dupa ID-ul sau specific
-const firstNameInput = document.querySelector('#fname');
-const lastNameInput = document.querySelector('#lname');
-const emailInput = document.querySelector('#email');
-const phoneNumberInput = document.querySelector('#phnumb');
+const firstNameInput = document.querySelector("#fname");
+const lastNameInput = document.querySelector("#lname");
+const emailInput = document.querySelector("#email");
+const phoneNumberInput = document.querySelector("#phnumb");
 // Targhetam Submit button
-const submitButton = document.querySelector('#buttonDiv input');
+const submitButton = document.querySelector("#buttonDiv input");
 
 // Targhetam span-urile cu mesaje
-const firstNameSpan = document.querySelector('#fnameSpan');
+const firstNameSpan = document.querySelector("#fnameSpan");
+
+//Targhetam forma
+const submitBtn = document.querySelector('#buttonDiv input');
 
 // Cream functiile de validare
 function letterCharValidation(input) {
     var value = input.value;
     var span = input.nextElementSibling;
-    console.log(span);
+    var label = input.previousElementSibling;
     var namePattern = /^[a-zA-Z\s-]+$/;
-    if (value === '') {
-        span.textContent = 'This field must be filled out';
-        span.classList.remove('hidden');
-        input.style.cssText = 'border: 2px solid red';
+    if (value === "") {
+        span.textContent = "This field is required";
+        label.style.cssText = "color: red";
+        failClasslistsToggle(input);
         return false;
     } else {
         if (!namePattern.test(value)) {
-            span.textContent = 'You must use only letters';
-            span.classList.remove('hidden');
-            input.style.cssText = 'border: 2px solid red';
+            span.textContent = "Please insert alphabetic characters only";
+            label.style.cssText = "color: red";
+            failClasslistsToggle(input);
             return false;
         } else {
-            console.log('All correct!');
-            span.textContent = '';
-            span.classList.add('hidden');
-            input.style.cssText = 'border: 2px solid green';
+            console.log("All correct!");
+            span.textContent = "";
+            label.style.cssText = "color: #370050";
+            successClasslistToggle(input);
             return true;
         }
     }
+}
 
-
-
-    // Regex verification
-
-    /*if (!firstName || !lastName) {
-        return "First name and last name are required.";
+function successClasslistToggle(el) {
+    var message = el.nextElementSibling;
+    if (!message.classList.contains("hidden")) {
+        message.classList.add("hidden");
+    } else {
     }
-
-    if (!namePattern.test(firstName) || !namePattern.test(lastName)) {
-        return "Invalid first name or last name.";
+    if (el.classList.contains("failInput")) {
+        el.classList.remove("failInput");
+    } else {
     }
+    if (!el.classList.contains("successInput")) {
+        el.classList.add("successInput");
+    } else {
+    }
+}
 
-    return "Valid name.";*/
-
-    // x - valoarea unui input
-    // Scenariul cu input gol
-    /*if (x == "") {
-    alert("Name must be filled out");
-    return false;
-  }*/
-
+function failClasslistsToggle(el) {
+    var message = el.nextElementSibling;
+    if (message.classList.contains("hidden")) {
+        message.classList.remove("hidden");
+    }
+    if (el.classList.contains("successInput")) {
+        el.classList.remove("successInput");
+    } else {
+    }
+    if (!el.classList.contains("failInput")) {
+        el.classList.add("failInput");
+    }
 }
 
 function emailValidation(input) {
-     var value = input.value;
-     var firstRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    var namePattern = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-    if (value === '') {
-        console.log('This field must be filled out');
-        input.style.cssText = 'border: 2px solid red';
+    var value = input.value;
+    var span = input.nextElementSibling;
+    var label = input.previousElementSibling;
+    var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (value.match(emailPattern)) {
+        span.textContent = "";
+        label.style.cssText = "color: #370050";
+        successClasslistToggle(input);
+        return true;
+    } else if (!email.value.includes('@') && email.value !== '') {
+        span.textContent = `Please include an '@' in the email address. ${value} is missing an '@'.`;
+        label.style.cssText = "color: red";
+        failClasslistsToggle(input);
+        return false;
+    } else if (email.value.endsWith('@')) {
+        span.textContent = `Please enter a part following '@'. '${value}' is incomplete.`;
+        label.style.cssText = "color: red";
+        failClasslistsToggle(input);
+        return false;
+    } else if (email.value == '') {
+        span.textContent = "This field is required";
+        label.style.cssText = "color: red";
+        failClasslistsToggle(input);
         return false;
     } else {
-        if (!namePattern.test(value)) {
-            console.log('You must use only letters');
-            input.style.cssText = 'border: 2px solid red';
-            return false;
-        } else {
-            console.log('All correct!');
-            input.style.cssText = 'border: 2px solid green';
-            return true;
-        }
+        span.textContent = `Please enter a domain name (I.e ".com", ".uk", ".net"). '${value}' is incomplete.`;
+        label.style.cssText = "color: red";
+        failClasslistsToggle(input);
+        return false;
     }
-
 }
 
 function phoneNumberValidation(input) {
     var value = input.value;
-    var letterPattern = /^[a-zA-Z\s-]+$/;
-    var namePattern = /^\d{9}$/;
-    if (value === '') {
-        console.log('This field must be filled out');
-        input.style.cssText = 'border: 2px solid red';
+    var span = input.nextElementSibling;
+    var label = input.previousElementSibling;
+    var phonePattern = /^0[0-9].*$/;
+    var lettersPattern = /^[a-zA-Z\s-]+$/;
+    if (value === "") {
+        span.textContent = "This field is required";
+        label.style.cssText = "color: red";
+        failClasslistsToggle(input);
         return false;
     } else {
-        if (!namePattern.test(value)) {
-            console.log('Incorrect number');
-            phoneNumberInput.value = '';
-            console.log('Scenariul cu stergerea literelor');
-            /*input.style.cssText = 'border: 2px solid red';
-            return false;*/
-        } /*else if (letterPattern.test(value)) {
-            phoneNumberInput.value = '';
-            console.log('Scenariul cu stergerea literelor');
-        }*/ else {
-            console.log('All correct!');
-            input.style.cssText = 'border: 2px solid green';
+        if (value.match(phonePattern) && value.length == 9 && value.startsWith('0')) {
+            console.log("All correct!");
+            span.textContent = "";
+            label.style.cssText = "color: #370050";
+            successClasslistToggle(input);
             return true;
+        } else {
+            if (lettersPattern.test(value)) {
+                span.textContent = 'Invalid phone number format. Please use only numbers.';
+            } else {
+                span.textContent = 'Please use a valid phone number. Number should be 9 digits and start with "0" (I.e 060111222).';
+            }
+            label.style.cssText = "color: red";
+            failClasslistsToggle(input);
+            return false;
         }
     }
 }
 
-firstNameInput.addEventListener('input', () => {
+
+
+firstNameInput.addEventListener("input", () => {
     letterCharValidation(firstNameInput);
 });
 
-lastNameInput.addEventListener('input', () => {
+lastNameInput.addEventListener("input", () => {
     letterCharValidation(lastNameInput);
 });
 
-emailInput.addEventListener('input', () => {
+emailInput.addEventListener("input", () => {
     emailValidation(emailInput);
 });
 
-phoneNumberInput.addEventListener('blur', () => {
+phoneNumberInput.addEventListener("input", () => {
     phoneNumberValidation(phoneNumberInput);
 });
+
+function validateAll() {
+    console.log('validating all');
+    letterCharValidation(firstNameInput);
+    letterCharValidation(lastNameInput);
+    emailValidation(emailInput);
+    phoneNumberValidation(phoneNumberInput);
+}
+
+submitBtn.addEventListener('click', validateAll);
